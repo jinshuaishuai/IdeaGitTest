822404c3b2d05da68fb22a3b92a7d388b350d39a
jinshuaishuai <jin2111843364@163.com>
2020-03-08 02:10:18 +0800

    添加SpringBoot相关配置

---
+++ springboot-jpa/src/main/java/com/jin/JPAStartUp.java
@@ -0,0 +1,11 @@
+package com.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPAStartUp {
+    public static void main(String[] args) {
+        SpringApplication.run(JPAStartUp.class, args);
+    }
+}

90dabb66911a89c59ca10667317d885c09c12196
jinshuaishuai <jin2111843364@163.com>
2020-03-08 02:26:57 +0800

    提交代码

---
+++ springboot-jpa/src/main/java/com/jin/controller/BookController.java
@@ -0,0 +1,12 @@
+package com.jin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/book")
+@Slf4j
+public class BookController {
+
+}
---
+++ springboot-jpa/src/main/java/com/jin/entity/AO/BookAO.java
@@ -0,0 +1,25 @@
+package com.jin.entity.AO;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class BookAO {
+
+    private Integer id;
+
+    private String bookName;
+
+    private BigDecimal bookPrice;
+
+    private String address;
+
+    private String phone;
+
+    private Integer isDelete;
+
+    private String createTime;
+
+    private String updateTime;
+}

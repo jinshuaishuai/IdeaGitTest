6f6c338efa0e20fd95ecabd76cd6232a9a0880ee
jinshuaishuai <jin2111843364@163.com>
2020-04-20 15:30:23 +0800

    使用zookeeper实现分布式锁，解决分布式应用中，程序重复执行一段代码或生成重复的订单号

---
+++ springboot-zookeeper/src/main/java/com/jin/ZookeeperApplication.java
@@ -0,0 +1,12 @@
+package com.jin;
+
+/**
+ * @author shuai.jin
+ * @description Zookeeper实现分布式锁
+ * @Date 2020/4/20 15:21
+ */
+public class ZookeeperApplication {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}

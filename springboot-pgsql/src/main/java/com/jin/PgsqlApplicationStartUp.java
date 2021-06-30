c5405d5bae1156747ed7ee3f64a22e69b8ea86e2
靳帅 <jin2111843364@163.com>
2021-06-30 14:30:12 +0800

    项目启动成功向zookeeper上注入信息

--- springboot-pgsql/src/main/java/com/jin/PgsqlApplicationStartUp.java
+++ springboot-pgsql/src/main/java/com/jin/PgsqlApplicationStartUp.java
@@ -1,6 +1,10 @@
 package com.jin;
 
+import org.I0Itec.zkclient.ZkClient;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,8 +17,28 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @MapperScan(basePackages = "com.jin.mapper.**")
-public class PgsqlApplicationStartUp {
+public class PgsqlApplicationStartUp implements ApplicationRunner {
+
+    @Autowired
+    private ZkClient zkClient;
+
     public static void main(String[] args) {
         SpringApplication.run(PgsqlApplicationStartUp.class, args);
+    }
+
+
+    /**
+     * 这个方法是实现于ApplicationRuuner接口的，目的是在项目启动之前，将服务实例注册到
+     * zookeeper上
+     */
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //项目启动的时候，向zookeeper上，写入实例信息
+        if(!zkClient.exists("/test")) {
+            zkClient.createPersistent("/test");
+        }
+
+        zkClient.createEphemeral("/test" + "/" + "pgsqlApplicationStartUp");
     }
 }
---
+++ springboot-pgsql/src/main/java/com/jin/config/ZookeeperConfiguration.java
@@ -0,0 +1,22 @@
+package com.jin.config;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Classname ZookeeperConfiguration
+ * @Description zookeeper配置类
+ * @Date 2021/6/30 14:12
+ * @Created by jinshuai
+ */
+@Configuration
+public class ZookeeperConfiguration {
+
+    @Bean
+    public ZkClient zkClient() {
+        String connectUrl = "localhost:2181";
+        ZkClient zkClient = new ZkClient(connectUrl);
+        return zkClient;
+    }
+}
